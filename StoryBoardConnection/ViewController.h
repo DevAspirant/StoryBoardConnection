a90a079d9bfe5a4f673c14b193d932fc31b525ef
ammar falmban <devAspirant@gmail.com>
2013-10-29 21:31:58 +0900

    Initial Commit

---
+++ StoryBoardConnection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardConnection
+//
+//  Created by ammar falmban on 10/29/13.
+//  Copyright (c) 2013 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoryBoardConnection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryBoardConnection
+//
+//  Created by ammar falmban on 10/29/13.
+//  Copyright (c) 2013 SDKaizen.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
